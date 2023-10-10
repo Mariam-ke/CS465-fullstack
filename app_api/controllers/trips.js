010fa6b7f599d3145ad4a13202fee1946124c742
Mariam-ke <mariam.haji01@gmail.com>
2023-10-10 21:23:21 +0300

    Applied SPA, created add, edit and delete trip buttons

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,14 +1,11 @@
-console.log("Inside tripsList controller");
-
 const mongoose = require('mongoose');
 const model = mongoose.model('trips');
 
 //GET: /trips - lists all the trips
 const tripsList = async (req, res) => {
     try {
-        let trips = await model.find({});
-        
-        if (!trips) {
+        let trips = await model.find({}).exec();
+        if (!trips || trips.length === 0) {
             return res.status(404).json({"message": "trip not found"});
         } else {
             return res.status(200).json(trips);
@@ -22,26 +19,79 @@ const tripsList = async (req, res) => {
 //GET /trips/:tripCode - returns a single trip
 
 const tripsFindByCode = async (req, res) => {
+    try {
+        let trip = await model.find({'code': req.params.tripCode}).exec();
+
+        if (!trip || trip.length === 0) {
+            return res.status(404).json({'message' : 'trip not found'});
+        } else {
+            return res.status(200).json(trip);
+        }
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+};
+
+//POST: /trips - adds a trip to the list of trips
+
+const tripsAddTrip = async (req, res) => {
+    try {
+        let trip = await model.create ({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+
+        });
+        return res.status(201).json(trip);
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+};
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
     model
-        .find({'code': req.params.tripCode})
-        .exec ((err, trip) => {
+        .findOneAndUpdate({'code': req.params.tripCode}, {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }, {new: true})
+        .then (trip => {
             if (!trip) {
                 return res
                     .status(404)
-                    .json({'message' : 'trip not found'});
-            } else if (err) {
+                    .send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
                 return res
                     .status(404)
-                    .json(err);
-            } else {
-                return res
-                    .status(200)
-                    .json(trip);
+                    .send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
             }
+            return res
+                .status(500)
+                .json(err);
         });
-};
+}
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
